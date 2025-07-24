4efa46a0dd553f622e5ae4fc575f593752e32714
sahithdaddla <daddlasahith>
2025-07-24 10:58:21 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -259,5 +259,5 @@ app.put('/api/leave-requests/:id', async (req, res) => {
 
 app.listen(port, async () => {
   await initializeDatabase();
-  console.log(`Server running at http://13.60.18.233:${port}`);
+  console.log(`Server running at http://13.61.174.97:${port}`);
 });
